50515364adc02aaa0924f6170d517dc21b181594
Jason <jghan011@gmail.com>
2020-11-12 00:18:15 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+//Component is a built in class inn ReactJS
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className = "App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+
     </div>
   );
+
 }
 
+}
+  
 export default App;
---
+++ src/Components/counter.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+
+class Counter  extends Component {
+    state = {
+        count:0
+        
+    };// is special object that includes any data the component needs
+    render() { 
+        return (<React.Fragment><span>{this.formatCount()}</span><button>Increment</button></React.Fragment> // jsx expression
+        //jsx expressions must have one parent element so wrapping it with a div is best or easiest
+        );
+
+       
+} 
+
+formatCount() {
+    const {count} = this.state; // now
+    return count === 0 ? 'Zero' : count;
+}
+}
+
+export default Counter;
+ // import react and component class from react module
+
+ // use parenthessis after return and place the div inside for successful code
+ // coommand + D to select multiple occurrences in vsc
+ //this refers to the object
